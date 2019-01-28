1eba7135d022a7d6b2b892596f70374a86a78d6e
ykiishida <ishida@minacolor.com>
2019-01-28 16:36:30 +0900

    mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
     before_action :correct_user,   only: [:edit, :update]
     before_action :admin_user,     only: :destroy
     
-  def index
-       @users = User.paginate(page: params[:page])
-  end
+    def index
+        @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    end
   
   def show
       @user = User.find(params[:id])
+      redirect_to root_url and return unless FILL_IN
   end
   
     
@@ -19,9 +20,9 @@ class UsersController < ApplicationController
   def create
       @user = User.new(user_params)
       if  @user.save
-          log_in @user
-          flash[:success] = "Welcome to the Sample App!"
-          redirect_to @user
+          @user.send_activation_email
+          flash[:info] = "Please check your email to activate your account."
+          redirect_to root_url
       else
           render'new'
       end
